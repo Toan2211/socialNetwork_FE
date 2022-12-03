9bfebc44249752aa9c3ee26e2b58938a0c33ea16
Toan2211 <nguyenthanhtoan2211@gmail.com>
2022-12-03 22:42:14 +0700

    fixBug/login_logout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Search from './Search'
 import Menu from './Menu'
+import Search from './Search'
 
-
-function Header() {
+const Header = () => {
     return (
-        <div className="header bg-light">
-            <nav className="navbar navbar-expand-lg navbar-light
-            bg-light justify-content-between align-middle">
-
+        <div className="header">
+            <nav className="navbar navbar-expand-lg navbar-light justify-content-between align-middle">
                 <Link to="/" className="logo">
-                    <h1 className="navbar-brand text-uppercase p-0 m-0"
-                        onClick={() => window.scrollTo({ top: 0 })}>
-                        TQS-Network
+                    <h1
+                        className="navbar-brand text-uppercase p-0 m-0"
+                        onClick={() => window.scrollTo({ top: 0 })}
+                    >
+                        TQS-Social
                     </h1>
                 </Link>
 
